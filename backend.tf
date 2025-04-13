2f82decbcc71ae434dd33d8205b1ee97b1002f6b
Hinaikram <hinaikramhh@gmail.com>
2025-04-13 11:13:07 +0000

    added simple

--- backend.tf
+++
@@ -1,7 +0,0 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "your-s3-bucket-name"
-#    key    = "eks/terraform.tfstate"
-#   region = "ap-south-1"
-# }
-#}
--- modules/eks/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "cluster_name" {}
-variable "kubernetes_version" {}
-variable "vpc_id" {}
-variable "private_subnets" {
-  type = list(string)
-}
